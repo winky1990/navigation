e36c7ea57b226864783fc79c2c99453ed1392889
winky <1845610800@qq.com>
2018-06-13 18:26:34 +0800

    Initial commit

---
+++ app/src/main/java/com/winky/navigation/MainActivity.java
@@ -0,0 +1,20 @@
+package com.winky.navigation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import androidx.navigation.Navigation;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return Navigation.findNavController(this, R.id.my_nav_host_fragment).navigateUp();
+    }
+}
---
+++ app/src/main/java/com/winky/navigation/MainPage2Fragment.java
@@ -0,0 +1,37 @@
+package com.winky.navigation;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.navigation.Navigation;
+
+public class MainPage2Fragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main_page2, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        view.findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(v).navigateUp();
+            }
+        });
+        view.findViewById(R.id.btn2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(v).navigate(R.id.action_page3);
+            }
+        });
+    }
+}
